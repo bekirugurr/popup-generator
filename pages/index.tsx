f558d791ad4d7f2cde1cba728530f1c5d3853cbd
bekirugurr <bekirugur070@gmail.com>
2022-09-26 07:46:49 +0300

    initial setup

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <>
+    <Head>
+      <title>Popup Generator</title>
+    </Head>
+    <div className="px-7 py-4">
+      <h1>Merhaba</h1>
+    </div>
+    </>
+  )
+}
+
+export default Home
